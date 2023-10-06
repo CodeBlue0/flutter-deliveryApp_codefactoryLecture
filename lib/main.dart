2fd8af87ed8cf4330cfe9fd6520821cc100c4d8f
CodeBlue0 <piri007@cau.ac.kr>
2023-10-06 13:23:15 +0900

    section 2

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:codefactory/common/view/splash_screen.dart';
+import 'package:codefactory/user/view/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const _App());
+}
+
+class _App extends StatelessWidget {
+  const _App();
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+            // fontFamily: 'NotoSans',
+            ),
+        debugShowCheckedModeBanner: false,
+        home: const SplashScreen());
+  }
+}
